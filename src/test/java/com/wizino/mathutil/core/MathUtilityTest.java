4df33897ea3203c353c161f259124050f059055e
Wizin25 <tanphu200213@gmail.com>
2023-10-12 08:47:39 +0700

    first-upload

---
+++ src/test/java/com/wizino/mathutil/core/MathUtilityTest.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.wizino.mathutil.core;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author tanph
+ */
+public class MathUtilityTest {
+
+    //TEST CAWSE TEMPLATE/STRUCTURE:
+    //ID | DESC | STEPS/PROCEDURES  | EXPECTED RESULT | STATUS(PASSED/FAIL)
+    //TEST CASE #1: Verify the getFactorial() function with n = 0
+    //Steps/Procedures :
+    //                  1. Given n = 0
+    //                  2. Call/invoke getFactorial(int 0)
+    //Expected Result
+    //                  The method getFactorial(n = 0) must return 1
+    //                              as the rusult of 0! == 1
+    //Status: PASSED | FAILED ĐOÁN XEM KHI CHẠY APP/HÀM/METHOD
+    //
+    //
+    @Test // dính dáng đến THUẬT NGỮ framwork 
+    public void verifyFactorialGivenRightArgument1ReturnsOk() {
+        assertEquals(1, 1); //chưa code nhưng template là thế
+
+    }
+    //Test CASE #2... n = 6
+        public void verifyFactorialGivenRightArgumentSixReturnsOk() {
+            assertEquals(2, 2); //chưa code nhưng template là thế
+        }
+
+   
+        
+}
