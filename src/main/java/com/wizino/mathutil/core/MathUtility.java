9a3a4477794ad9ea9e887983315355eed0518ac4
Wizin25 <tanphu200213@gmail.com>
2023-10-16 08:57:03 +0700

    update getf() to use recursion. see green4ever

--- src/main/java/com/wizino/mathutil/core/MathUtility.java
+++ src/main/java/com/wizino/mathutil/core/MathUtility.java
@@ -15,7 +15,17 @@ public class MathUtility {
     //0! = 1! = 1 quy ước 0! là 1
     //vì giai thừa tăng rất nhanh, 21! kiểu long chứa ko nổi (vượt 18 số 0)
     //do đó hàm getF() của chúng ta chỉ chơi với n = 0..20
-    public static long getFactorial(int n) {
+    
+    
+    //5! = 1.2.3.4.5
+    //5! = 4! x5 = 5 x 4!
+    //4! = 4 x 3!
+    //3! = 3 x 2!
+    //2! = 2 x 1! - DỪNG LẠI NHEN, KO ĐUỔI NHAU NỮA
+    //N! = N x(N - 1)! -> CÔNG THỨC ĐỆ QUY XUẤT HIỆN
+    //ĐỆ QUY - RECURSION - GỌI LẠI CHÍNH MÌNH VỚI QUY MÔ NHỎ HƠN
+    
+      public static long getFactorial(int n) { //N!
 
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid n. n must be 0..20");
@@ -23,14 +33,42 @@ public class MathUtility {
 
         if (n == 0 || n == 1) 
             return 1;   //nếu n đặc biệt, ta return sớm luôn
+        return n * getFactorial(n-1);
         
-        long result = 10; //sống sót đến lệnh này, sure n = 2..20
-
-        for (int i = 2; i <= n; i++) 
-            result *= i;
-        
-        return result;
+       // tối ưu code, thay ruôt của cái hàm để chạy ngon hơn
+       // nhanh hơn, hoặc code viết đẹp hơn
+       // hoặc code bug thì sửa code
+       //DÙ BẠN SỬA CODE VÌ LÝ DO GÌ, THÌ CODE PHẢI ĐƯỢC TEST LẠI
+       //VÀ TEST LẠI ĐỂ ĐẢM BẢO RẰNG NÓ VẪN XANH NHƯ LẦN TRƯỚC
+       //VẬY NẾU TA VIẾT CODE ĐI KÈM TEST SCRIPT THÌ VIỆC TEST LẠI CÁI HÀM
+       //SẼ CỰC NHANH, KO CẦN DÙNG MẮT ĐỂ DÒ TỪNG DÒNG LỆNH ĐÚNG SAI
+       //MÀ CHỈ NHÌN MAU XANH LÀ ĐỦ - VÌ HÀM TRẢ VỀ NHƯ EXPECTED
+       //TEST LẠI NHỮNG GÌ ĐÃ TỪNG TEST -> REGRESSION TEST
+       //                                   KIỂM THỬ HỒI QUY
+       //KIỂM THỬ LẠI HÀM CỰC NHANH VÌ GITHUB ACTION LO GIÙM
+       //TA CHỈ CẦN MÀU XANH, HOẶC NHẬN EMAIL CHỬI VÌ CODE ĐỎ
 
     }
+
+    
+    
+    
+//    public static long getFactorial(int n) {
+//
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("Invalid n. n must be 0..20");
+//        }
+//
+//        if (n == 0 || n == 1) 
+//            return 1;   //nếu n đặc biệt, ta return sớm luôn
+//        
+//        long result = 1; //sống sót đến lệnh này, sure n = 2..20
+//
+//        for (int i = 2; i <= n; i++) 
+//            result *= i;
+//        
+//        return result;
+//
+//    }
 
 }
